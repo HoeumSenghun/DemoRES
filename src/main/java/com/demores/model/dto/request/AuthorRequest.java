e0e2dee4545ca30834fd3f242784b7f902020f2c
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-04-05 01:12:20 +0700

    add Author request body

--- src/main/java/com/demores/model/dto/request/AuthorRequest.java
+++ src/main/java/com/demores/model/dto/request/AuthorRequest.java
@@ -1,4 +1,13 @@
 package com.demores.model.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthorRequest {
+    private String name;
+    private String gender;
 }
