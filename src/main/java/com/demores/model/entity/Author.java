0db7f7fabd9a3d1fcb82ecc60f0b4f2a8b8edbc6
HoeumSenghun <hoeumsenghun369@gmail.com>
2025-04-05 01:12:02 +0700

    add Author entity

--- src/main/java/com/demores/model/entity/Author.java
+++ src/main/java/com/demores/model/entity/Author.java
@@ -1,4 +1,15 @@
 package com.demores.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Author {
+    private Integer id;
+    private String name;
+    private String gender;
 }
